d5515bb99b5b2032a9b64a041270a16adaa9a80d
kretatusha <92570098+kretatusha@users.noreply.github.com>
2022-04-14 23:14:08 +0500

    Initial commit

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+import time
+
+from ntplib import NTPStats
+
+stats = NTPStats()
+
+sock = socket.socket()
+sock.connect(('localhost', 123))
+sock.send(str('HLO').encode())
+
+data = sock.recv(1024)
+sock.close()
+
+print('Полученные данные: {}'.format(data))
+stats.from_data(data)
+print('Время полученное пользователем: {}'.format(time.ctime(stats.tx_time)))
